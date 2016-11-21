0ef46e089b9d346a6ad66776310c9c0414c33210
zackrdavis <zackrdavis@gmail.com>
2016-11-21 14:36:09 -0500

    track typing in state

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let id = 0
 
 // chat history
-const MessageList = ({messages}) => {
+const MessageList = ({state}) => {
+  let messages = state.data;
+  let typing = state.typing;
+
   const list = messages.map((message) => {
     return (<li key={message.id}>{message.sender} {message.text}</li>)
   });
+
   return (<ul>{list}</ul>);
 }
 
 // text input
-const MessageForm = ({sendMessage, sender}) => {
+const MessageForm = ({sendMessage, sendTyping, sender}) => {
   let input;
+
   return (
     <div>
       <input ref={node => {
@@ -18,6 +23,7 @@ const MessageForm = ({sendMessage, sender}) => {
       }} />
       <button onClick={() => {
         sendMessage(input.value, sender);
+        sendTyping(sender);
         input.value = '';
       }}>
         +
@@ -26,25 +32,28 @@ const MessageForm = ({sendMessage, sender}) => {
   )
 }
 
-// window for each user
-const ChatWindow = ({sender, sendMessage, messages}) => {
+// single-user chat window
+const ChatWindow = ({sender, sendMessage, sendTyping, state}) => {
+  let senderIsTyping = sender === state.typing
+
   return (
     <div>
       <div>{sender}</div>
-      <MessageList messages={messages} />
-      <MessageForm sender={sender} sendMessage={sendMessage} />
+      <MessageList state={state} />
+      <MessageForm sendMessage={sendMessage} sendTyping={sendTyping} sender={sender} />
     </div>
   )
 }
 
 
-class ChatApp extends React.Component{
+class ChatApp extends React.Component {
   constructor(props) {
     // pass props to parent class
     super(props);
     // set initial state
     this.state = {
-      data: []
+      data: [],
+      typing: false
     }
   }
 
@@ -52,24 +61,36 @@ class ChatApp extends React.Component{
     // Assemble data
     const message = {sender:sender, text: val, id: id++}
     // Update data
-    this.state.data.push(message);
+    let messages = this.state.data
+    messages.push(message);
     // Update state
     this.setState({data: this.state.data});
   }
 
+  sendTyping(sender) {
+    // Update data
+    this.setState({typing: sender});
+    setTimeout( () => {
+      this.setState({typing: false});
+    }, 1000)
+  }
+
   render() {
+    // chat window for each user
     return (
       <div>
         <ChatWindow
-          sender='Laura'
-          sendMessage={this.sendMessage.bind(this)}
-          messages={this.state.data}
+          sender = 'Laura'
+          sendMessage = {this.sendMessage.bind(this)}
+          sendTyping = {this.sendTyping.bind(this)}
+          state = {this.state}
         />
 
         <ChatWindow
-          sender='Rob'
-          sendMessage={this.sendMessage.bind(this)}
-          messages={this.state.data}
+          sender = 'Rob'
+          sendMessage = {this.sendMessage.bind(this)}
+          sendTyping = {this.sendTyping.bind(this)}
+          state = {this.state}
         />
       </div>
     )
