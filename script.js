1d6fe7b59bf25987a8eda295b0810be4dafc557d
zackrdavis <zackrdavis@gmail.com>
2016-11-22 14:22:13 -0500

    reorder js

--- script.js
+++ script.js
@@ -1,6 +1,112 @@
-let id = 0
+// coordinates ChatWindow-ChatWindow or ChatWindow-Server
+class ChatApp extends React.Component {
+  constructor(props) {
+    // pass props to parent class
+    super(props);
+    // set initial state
+    this.state = {
+      users: [
+        'Laura',
+        'Rob',
+        // 'Jim'
+      ],
+      messages: [],
+      suggestions: {},
+      // either false or name of user
+      typing: false
+    }
+
+    // init cleverBot
+    this.bot = new cleverbot('lhH9sJn4zuuq2MTK', 'bKLf8nLE8EbXpFDFqdqr2J4svPOjMVYn');
+    this.bot.setNick('react-chat');
+    this.bot.create((err, session) => {
+      //console.log('bot is ready');
+    })
+  }
+
+  consultBot(user, message) {
+    // TODO: error handling
+    this.bot.ask(message, (err, firstResponse) => {
+      this.bot.ask(message, (err, secondResponse) => {
+        this.state.suggestions[user] = []
+        // push first response
+        this.state.suggestions[user].push(firstResponse);
+        // push second response only if it's unique
+        if(secondResponse !== firstResponse) {
+          this.state.suggestions[user].push(secondResponse);
+        }
+        // push another helpful answer
+        this.state.suggestions[user].push('One moment while I consult my manager...');
+        // set state
+        this.setState({ suggestions: this.state.suggestions })
+      })
+    });
+  }
+
+
+  triggerSuggestions(sender, message) {
+    let users = this.state.users;
+
+    // for all users other than sender, consult bot for responses
+    for (let i = 0; i < users.length; i++) {
+      if(users[i] !== sender) {
+        this.consultBot(users[i], message)
+      }
+    }
+  }
+
+  removeSentSuggestion(user, val) {
+    let userSuggestions = this.state.suggestions[user];
+
+    // if sent message matches a suggestion, remove the suggestion
+    if(userSuggestions && userSuggestions.indexOf(val) !== -1) {
+      userSuggestions.splice(userSuggestions.indexOf(val), 1);
+      this.setState({ suggestions: this.state.suggestions })
+    }
+  }
+
+  sendMessage(val, user) {
+    // Assemble message data
+    const message = {user:user, text: val, id: id++}
+
+    // Update message data
+    this.state.messages.push(message);
+    this.setState({messages: this.state.messages});
+
+    this.triggerSuggestions(user, val)
+    this.removeSentSuggestion(user, val)
+  }
+
+  sendTyping(user) {
+    this.setState({typing: user});
+    // TODO: debounce
+    setTimeout( () => {
+      this.setState({typing: false});
+    }, 1000)
+  }
+
+  render() {
+    // build chat window for each user
+    const windowList = this.state.users.map((user) => {
+      return (
+        <ChatWindow
+          user = {user}
+          sendMessage = {this.sendMessage.bind(this)}
+          sendTyping = {this.sendTyping.bind(this)}
+          state = {this.state}
+        />
+      )
+    });
+
+    return (
+      <div>{windowList}</div>
+    )
+  }
+}
+
 
 // chat history
+let id = 0
 const MessageList = ({user, state}) => {
   let messages = state.messages;
   let typing = state.typing;
@@ -63,6 +169,7 @@ const MessageForm = ({sendMessage, sendTyping, user}) => {
 }
 
 
+// bot-generated suggestions list
 const SuggestionList = ({sendMessage, suggestions, user}) => {
   // build list if suggestions exist
   if(suggestions) {
@@ -97,112 +204,6 @@ const ChatWindow = ({user, sendMessage, sendTyping, state}) => {
       </div>
     </div>
   )
-}
-
-// coordinates ChatWindow-ChatWindow or ChatWindow-Server
-class ChatApp extends React.Component {
-  constructor(props) {
-    // pass props to parent class
-    super(props);
-    // set initial state
-    this.state = {
-      users: [
-        'Laura',
-        'Rob',
-        // 'Jim'
-      ],
-      messages: [],
-      suggestions: {},
-      // either false or name of user
-      typing: false
-    }
-
-    // init cleverBot
-    this.bot = new cleverbot('lhH9sJn4zuuq2MTK', 'bKLf8nLE8EbXpFDFqdqr2J4svPOjMVYn');
-    this.bot.setNick('react-chat');
-    this.bot.create(function (err, session) {
-      //console.log('bot is ready');
-    })
-  }
-
-  consultBot(user, message) {
-    // TODO: error handling
-    this.bot.ask(message, (err, firstResponse) => {
-      this.bot.ask(message, (err, secondResponse) => {
-        this.state.suggestions[user] = []
-        // push first response
-        this.state.suggestions[user].push(firstResponse);
-        // push second response only if it's unique
-        if(secondResponse !== firstResponse) {
-          this.state.suggestions[user].push(secondResponse);
-        }
-        // push another helpful answer
-        this.state.suggestions[user].push('One moment while I consult my manager...');
-        // set state
-        this.setState({ suggestions: this.state.suggestions })
-      })
-    });
-  }
-
-
-  triggerSuggestions(sender, message) {
-    let users = this.state.users;
-
-    // for all users other than sender, consult bot for responses
-    for (let i = 0; i < users.length; i++) {
-      if(users[i] !== sender) {
-        this.consultBot(users[i], message)
-      }
-    }
-  }
-
-  removeSentSuggestion(user, val) {
-    let userSuggestions = this.state.suggestions[user];
-
-    // if sent message matches a suggestion, remove the suggestion
-    if(userSuggestions && userSuggestions.indexOf(val) !== -1) {
-      userSuggestions.splice(userSuggestions.indexOf(val), 1);
-      this.setState({ suggestions: this.state.suggestions })
-    }
-  }
-
-  sendMessage(val, user) {
-    // Assemble message data
-    const message = {user:user, text: val, id: id++}
-
-    // Update message data
-    this.state.messages.push(message);
-    this.setState({messages: this.state.messages});
-
-    this.triggerSuggestions(user, val)
-    this.removeSentSuggestion(user, val)
-  }
-
-  sendTyping(user) {
-    // Update data
-    this.setState({typing: user});
-    setTimeout( () => {
-      this.setState({typing: false});
-    }, 1000)
-  }
-
-  render() {
-    // build chat window for each user
-    const windowList = this.state.users.map((user) => {
-      return (
-        <ChatWindow
-          user = {user}
-          sendMessage = {this.sendMessage.bind(this)}
-          sendTyping = {this.sendTyping.bind(this)}
-          state = {this.state}
-        />
-      )
-    });
-
-    return (
-      <div>{windowList}</div>
-    )
-  }
 }
 
 ReactDOM.render(<ChatApp />, document.getElementById('chat-app'))
