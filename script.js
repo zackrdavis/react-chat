4bf22a3b4f51c46ec39ec0debd9c2ddccf3d4cdc
zackrdavis <zackrdavis@gmail.com>
2016-11-22 02:54:57 -0500

    consult cleverbot api for suggestions

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let id = 0
 
 // chat history
-const MessageList = ({sender, state}) => {
+const MessageList = ({user, state}) => {
   let messages = state.messages;
   let typing = state.typing;
 
   // classname for final li
-  let typingClass = typing && typing !== sender ? 'elipses typing' : 'elipses'
+  let typingClass = typing && typing !== user ? 'elipses typing' : 'elipses'
 
   const list = messages.map((message) => {
-    return (<div key={message.id}>{message.sender} {message.text}</div>)
+    return (<div key={message.id}>{message.user} {message.text}</div>)
   });
 
   return (
@@ -20,12 +20,13 @@ const MessageList = ({sender, state}) => {
   );
 }
 
+
 // text input
-const MessageForm = ({sendMessage, sendTyping, sender}) => {
+const MessageForm = ({sendMessage, sendTyping, user}) => {
   let input;
 
   let handleChange = () => {
-    sendTyping(sender)
+    sendTyping(user)
   }
 
   return (
@@ -34,7 +35,7 @@ const MessageForm = ({sendMessage, sendTyping, sender}) => {
         input = node;
       }} />
       <button onClick={() => {
-        sendMessage(input.value, sender);
+        sendMessage(input.value, user);
         input.value = '';
       }}>
         +
@@ -43,67 +44,103 @@ const MessageForm = ({sendMessage, sendTyping, sender}) => {
   )
 }
 
+
 // single-user chat window
-const ChatWindow = ({sender, sendMessage, sendTyping, state}) => {
+const ChatWindow = ({user, sendMessage, sendTyping, getSuggestions, state}) => {
   return (
     <div className="chat-outer">
       <div className="chat-inner">
-        <div className="chat-header">{sender}</div>
-        <MessageList sender={sender} state={state} />
-        <MessageForm sendMessage={sendMessage} sendTyping={sendTyping} sender={sender} />
+        <div className="chat-header">{user}</div>
+        <MessageList user={user} state={state} />
+        <MessageForm sendMessage={sendMessage} sendTyping={sendTyping} user={user} />
       </div>
     </div>
   )
 }
 
-
+// coordinates ChatWindow-ChatWindow or ChatWindow-Server
 class ChatApp extends React.Component {
   constructor(props) {
     // pass props to parent class
     super(props);
     // set initial state
     this.state = {
+      users: [
+        'Laura',
+        'Rob',
+        //'Jim'
+      ],
       messages: [],
-      // either false or name of sender
+      suggestions: {},
+      // either false or name of user
       typing: false
+    }
+
+    // init cleverBot
+    this.bot = new cleverbot('lhH9sJn4zuuq2MTK', 'bKLf8nLE8EbXpFDFqdqr2J4svPOjMVYn');
+    this.bot.setNick('react-chat');
+    this.bot.create(function (err, session) {
+      //console.log('bot is ready');
+    })
+  }
+
+  consultBot(user, message) {
+    console.log(message);
+
+    this.bot.ask(message, (err, response) => {
+      this.state.suggestions[user] = response;
+      this.setState({ suggestions: this.state.suggestions })
+    });
+
+  }
+
+
+  triggerSuggestions(sender, message) {
+    let users = this.state.users;
+
+    // for all users other than sender, consult bot for responses
+    for (let i = 0; i < users.length; i++) {
+      if(users[i] !== sender) {
+        console.log(message);
+        this.consultBot(users[i], message)
+      }
     }
   }
 
-  sendMessage(val, sender) {
-    // Assemble data
-    const message = {sender:sender, text: val, id: id++}
-    // Update data
+  sendMessage(val, user) {
+    // Assemble message data
+    const message = {user:user, text: val, id: id++}
+
+    // Update message data
     this.state.messages.push(message);
-    // Update state
     this.setState({messages: this.state.messages});
+
+    this.triggerSuggestions(user, val)
   }
 
-  sendTyping(sender) {
+  sendTyping(user) {
     // Update data
-    this.setState({typing: sender});
+    this.setState({typing: user});
     setTimeout( () => {
       this.setState({typing: false});
     }, 1000)
   }
 
   render() {
-    // chat window for each user
-    return (
-      <div>
+    // build chat window for each user
+    const windowList = this.state.users.map((user) => {
+      return (
         <ChatWindow
-          sender = 'Laura'
+          user = {user}
           sendMessage = {this.sendMessage.bind(this)}
           sendTyping = {this.sendTyping.bind(this)}
           state = {this.state}
         />
+      )
+    });
 
-        <ChatWindow
-          sender = 'Rob'
-          sendMessage = {this.sendMessage.bind(this)}
-          sendTyping = {this.sendTyping.bind(this)}
-          state = {this.state}
-        />
-      </div>
+    return (
+      <div>{windowList}</div>
     )
   }
 }
